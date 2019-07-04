72d1bd209d310ee8e03983cdaaf388fbc8d4bde8
likhithadakuri <52519842+likhithadakuri@users.noreply.github.com>
2019-07-04 15:40:33 +0530

    Update new2.java

--- src/new2.java
+++ src/new2.java
@@ -9,6 +9,7 @@ public static void main(String args)
 	System.out.print("hi");
 	System.out.print("hi123");
 	System.out.print("hello");
+	System.out.print("hello");System.out.print("hello");System.out.print("hello");
 	
 }
 
