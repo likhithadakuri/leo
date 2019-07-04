6918bc19c97eca4dfcbcd0ac0faaf3378a3d3876
user <user@itilite-OptiPlex-5060>
2019-07-04 12:10:04 +0530

    changes

--- src/new1.java
+++ src/new1.java
@@ -2,7 +2,9 @@
 public class new1 {
 	public static void main(String[] args)
 	{
-		 System.out.println("Likhitha");	
+		 System.out.println("Likhitha");
+		 
+		 int a=2;
 	}
 
 }
---
+++ src/new2.java
@@ -0,0 +1,10 @@
+
+public class new2 {
+public static void main(String args)
+{
+	int a=5,b=8;
+			int c=a+b;
+			System.out.println(c);
+}
+
+}
